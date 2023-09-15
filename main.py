599529ef220a30265f2ac6b5d4705f2990981e16
Kush Patel <patelkush1508@gmail.com>
2023-09-14 23:33:59 -0400

    initialization for Fire, Bot, Button

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import random
 
-D = 100
-grid = [["B" for _ in range(D)] for _ in range(D)]
+# Bock - #
+# Open - O
+# Bot - P
+# Button - B
+# Fire - F
+
+D = 4
+grid = [["#" for _ in range(D)] for _ in range(D)]
 openBlockPosArr = []
 deadEndCells = []
+openGrid = []
 
-randIntX = random.randint(1, D-2)
-randIntY = random.randint(1, D-2)
+randIntX = random.randint(1, D - 2)
+randIntY = random.randint(1, D - 2)
 grid[randIntX][randIntY] = "O"
 openBlockPosArr.append((randIntX, randIntY))
 
@@ -16,14 +23,15 @@ effY1 = randIntY - 1
 effY2 = randIntY + 1
 
 # debug info
-print("picked: " + "(" + str(randIntX) + ", " + str(randIntY) + ")")
+# print("picked: " + "(" + str(randIntX) + ", " + str(randIntY) + ")")
+
 
 while openBlockPosArr:
     openBlockPosArr = []
     for x in range(effX1, effX2 + 1):
         for y in range(effY1, effY2 + 1):
             openBlockPos = 0
-            if grid[x][y] == "B":
+            if grid[x][y] == "#":
                 a = x + 1
                 b = x - 1
                 c = y + 1
@@ -41,9 +49,9 @@ while openBlockPosArr:
     if openBlockPosArr:
 
         # debug info
-        print()
-        print(openBlockPosArr)
-        print()
+        # print()
+        # print(openBlockPosArr)
+        # print()
 
         randIndex = random.randint(0, len(openBlockPosArr) - 1)
         randIntX, randIntY = openBlockPosArr[randIndex]
@@ -51,39 +59,18 @@ while openBlockPosArr:
 
         if 0 < effX1 == randIntX:
             effX1 = randIntX - 1
-        if D-1 > effX2 == randIntX:
+        if D - 1 > effX2 == randIntX:
             effX2 = randIntX + 1
         if 0 < effY1 == randIntY:
             effY1 = randIntY - 1
-        if D-1 > effY2 == randIntY:
+        if D - 1 > effY2 == randIntY:
             effY2 = randIntY + 1
 
         # debug info
-        print()
-        for x in grid:
-            print(' '.join(x))
-        print()
-
-for x in range(D):
-    for y in range(D):
-        deadEndBlock = 0
-        if grid[x][y] == "O":
-            a = x + 1
-            b = x - 1
-            c = y + 1
-            d = y - 1
-            if 0 <= a <= D - 1 and grid[a][y] == "B":
-                deadEndBlock += 1
-            if 0 <= b <= D - 1 and grid[b][y] == "B":
-                deadEndBlock += 1
-            if 0 <= c <= D - 1 and grid[x][c] == "B":
-                deadEndBlock += 1
-            if 0 <= d <= D - 1 and grid[x][d] == "B":
-                deadEndBlock += 1
-            if deadEndBlock >= 3:
-                deadEndCells.append((x, y))
-
-
+        # print()
+        # for x in grid:
+        #     print(' '.join(x))
+        # print()
 
 for x in range(D):
     for y in range(D):
@@ -94,16 +81,16 @@ for x in range(D):
             b = x - 1
             c = y + 1
             d = y - 1
-            if 0 <= a <= D - 1 and grid[a][y] == "B":
+            if 0 <= a <= D - 1 and grid[a][y] == "#":
                 deadEndBlock += 1
                 deadEndBlockArr.append((a, y))
-            if 0 <= b <= D - 1 and grid[b][y] == "B":
+            if 0 <= b <= D - 1 and grid[b][y] == "#":
                 deadEndBlock += 1
                 deadEndBlockArr.append((b, y))
-            if 0 <= c <= D - 1 and grid[x][c] == "B":
+            if 0 <= c <= D - 1 and grid[x][c] == "#":
                 deadEndBlock += 1
                 deadEndBlockArr.append((x, c))
-            if 0 <= d <= D - 1 and grid[x][d] == "B":
+            if 0 <= d <= D - 1 and grid[x][d] == "#":
                 deadEndBlock += 1
                 deadEndBlockArr.append((x, d))
             if deadEndBlock >= 3:
@@ -111,5 +98,91 @@ for x in range(D):
                 randIntX, randIntY = deadEndBlockArr[randIndex]
                 grid[randIntX][randIntY] = "O"
 
-# debug info
-print("Done Opening")
+# Appending Open Cell to OpenGrid List
+for x in range(D):
+    for y in range(D):
+        if grid[x][y] == "O":
+            openGrid.append((x, y))
+
+
+# Find the Position for The Fire Cell
+def fire_open_Startposition():
+    position = random.randint(0, len(openGrid) - 1)
+    a, b = openGrid[position]
+    openGrid.pop(position)
+    # print(a, b, position, grid[a][b], len(queue))
+    return a, b
+
+
+# Find the Position for The Button Cell
+def button_open_position():
+    position = random.randint(0, len(openGrid) - 1)
+    a, b = openGrid[position]
+    openGrid.pop(position)
+    # print(a, b, position, grid[a][b], len(queue))
+    return a, b
+
+
+# Find the Position for the Bot Cell
+def bot_open_Startposition():
+    position = random.randint(0, len(openGrid) - 1)
+    a, b = openGrid[position]
+    openGrid.pop(position)
+    # print(a, b, position, grid[a][b], len(queue))
+    return a, b
+
+
+def find_side_neighbour(x, y, factor):
+
+    if factor == 1:
+        Direction_to_check = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        direction = []
+        for a, b in Direction_to_check:
+            new_x, new_y = a + x, b + y
+            if (D > new_x > -1) and (-1 < new_y < D) and (
+                    grid[new_x][new_y] == "O" or grid[new_x][new_y] == "P" or grid[new_x][new_y] == "B"):
+                direction.append((new_x, new_y))
+        print('dire==', direction)
+    elif factor == 2:
+        Direction_to_check = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        direction = []
+        for a, b in Direction_to_check:
+            new_x, new_y = a + x, b + y
+            if (D > new_x > -1) and (-1 < new_y < D) and (grid[new_x][new_y] == "O" or grid[new_x][new_y] == "B"):
+                direction.append((new_x, new_y))
+        print('dire==', direction)
+
+    return direction
+
+
+def fire(x, y):
+    firePath = []
+    firePath.extend(find_side_neighbour(x, y, 1))
+    q = random.randint(0, len(firePath) - 1)
+    #print(firePath[q])
+    a,b = firePath[q]
+    return a,b
+
+def bot_1(x, y):
+    bothPath = []
+    bothPath.extend(find_side_neighbour(x, y, 2))
+    q = random.randint(0, len(bothPath) - 1)
+    #print(firePath[q])
+    a,b = bothPath[q]
+    return a,b
+
+x_bot, y_bot = bot_open_Startposition()
+x_button, y_button = button_open_position()
+x_fire, y_fire = button_open_position()
+
+grid[x_bot][y_bot] = "P"
+grid[x_button][y_button] = "B"
+grid[x_fire][y_fire] = "F"
+
+print(fire(x_fire, y_fire))
+
+print(bot_1(x_bot, y_bot))
+
+for x in grid:
+    print(' '.join(x))
+print()
